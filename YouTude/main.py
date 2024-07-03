b46b8d0f2f9ad2b201559af73e8d300dbd196cb6
Wasimultonmoy <79182688+Wasimultonmoy@users.noreply.github.com>
2024-07-03 10:33:56 +0600

    Added the app icon and Audio download option

--- YouTude/main.py
+++ YouTude/main.py
@@ -1,20 +1,28 @@
 import tkinter
 import customtkinter
 from pytube import YouTube
-
 #Functions
 def startDownload():
     try:
         ytlink = link.get()
         ytObject = YouTube(ytlink, on_progress_callback=on_progress)
-        video = ytObject.streams.get_highest_resolution()
-        title.configure(text = ytObject.title, text_color ="white")
-        finishlabel.configure(text ="")
-        video.download()
-        finishlabel.configure(text = "Downloaded!")
-    except:
-        finishlabel.columnconfigure(text = "Download Error", text_color = "red")
-
+        option = download_choice.get()
+        if option == 'Video':
+            video = ytObject.streams.get_highest_resolution()
+            title.configure(text = ytObject.title, text_color = "white")
+            finishlabel.configure(text = "")
+            video.download()
+            finishlabel.configure(text = "Downloaded!")
+        elif option == 'Audio':
+            audio = ytObject.streams.get_audio_only()
+            title.configure(text = ytObject.title, text_color = "white")
+            finishlabel.configure(text = "")
+            audio.download()
+            finishlabel.configure(text = "Downloaded!")
+    except Exception as e:
+        finishlabel.configure(text = "Download Error", text_color = "red")
+        print(e)
+        
 def on_progress(stream, chunks, bytes_remain):
     total_size = stream.filesize
     bytes_reaming = total_size - bytes_remain
@@ -30,13 +38,13 @@ def on_progress(stream, chunks, bytes_remain):
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
-
 #Our app Frame
 app = customtkinter.CTk()
 app.geometry("1280x720")
-app.title("Tonmoy yt_downloder")
-
-
+app.title("YouTube Downloader")
+#app icon
+icon = "Image/download.ico"
+app.iconbitmap(icon)
 #UI elements
 title = customtkinter.CTkLabel(app, text="Insert a YouTube Link")
 title.pack(padx = 10, pady = 10)
@@ -45,6 +53,13 @@ title.pack(padx = 10, pady = 10)
 url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width= 350, height= 40, textvariable= url_var)
 link.pack()
+
+#Video/Audio Options
+download_choice = tkinter.StringVar(value='Video')
+video_radio = customtkinter.CTkRadioButton(app, text="Video", variable=download_choice, value='Video')
+video_radio.pack(padx= 10, pady= 10)
+audio_radio = customtkinter.CTkRadioButton(app, text="Auido", variable=download_choice, value= 'Audio')
+audio_radio.pack(padx= 10, pady= 10)
 
 #Finish Label
 finishlabel = customtkinter.CTkLabel(app, text="")
